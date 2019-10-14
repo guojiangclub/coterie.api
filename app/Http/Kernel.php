b4088635f30e19c796fec9c9d11d3253b3e7fad6
tangwei666 <18670055879@163.com>
2019-10-14 15:11:30 +0800

    Update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'cors' => \Barryvdh\Cors\HandleCors::class,
+        'coterie_notification' => \iBrand\Coterie\Server\Http\Middleware\Notification::class,
     ];
 }
--- modules/Coterie.Core/src/config.php
+++ modules/Coterie.Core/src/config.php
@@ -11,7 +11,7 @@
 
 return [
     'routeAttributes' => [
-        'middleware' => ['api', 'cors'],
+        'middleware' => ['api', 'cors','coterie_notification'],
     ],
 
     'routeAuthAttributes' => [
