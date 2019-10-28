72002213e2e3d96bc9f695da91381298f9aa724d
tangwei666 <18670055879@163.com>
2019-10-28 10:56:45 +0800

    Update

--- modules/Coterie.Server/src/Http/Middleware/msgSecCheck.php
+++ modules/Coterie.Server/src/Http/Middleware/msgSecCheck.php
@@ -1,4 +1,4 @@
-<?php
+﻿﻿<?php
 
 namespace iBrand\Coterie\Server\Http\Middleware;
 
@@ -45,13 +45,21 @@ class msgSecCheck
         if($request->isMethod('post') AND count($request->all())){
 
             $input=json_encode($request->all(),JSON_UNESCAPED_UNICODE);
+            $pattern ='utf8'?'/[\x{4e00}-\x{9fa5}]/u':'/[\x80-\xFF]/';
+            preg_match_all($pattern,$input,$result);
+            $temp =join('',$result[0]);
 
-            $res=$this->miniProgram->msgSecCheck($input);
+            if(!empty($temp)){
 
-            if($res['errcode']!=0){
+                $res=$this->miniProgram->msgSecCheck($temp);
 
-                return response(['status' => false, 'code' => 400, 'message' =>'您的内容违反相关规定' ,'data' =>[]]);
+                if($res['errcode']!=0){
+
+                    return response(['status' => false, 'code' => 400, 'message' =>'您的内容违反相关规定' ,'data' =>[]]);
+                }
             }
+
+
         }
 
         return $next($request);
